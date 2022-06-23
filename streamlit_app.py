2696c4620a2a9437ae5b80ddab535f6425580ba3
Sreesha Achar HB <108048261+sreesha8798@users.noreply.github.com>
2022-06-23 11:26:21 +0530

    Update streamlit_app.py
    Added Subheaders and Menu Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
+streamlit.subheader('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smothie')
+streamlit.text('Har-boiled Free-range egg')
